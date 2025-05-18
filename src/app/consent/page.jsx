35f89a0a0da36339af6ecd6bac488438417bfbb6
Tanvir Ahamed <sowmik265@gmail.com>
2025-05-19 00:31:42 +0600

    color changes for button and consent page

--- src/app/consent/page.jsx
+++ src/app/consent/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function ConsentPage() {
@@ -11,7 +12,7 @@ export default function ConsentPage() {
   return (
     <div className="h-screen w-full bg-white flex flex-col justify-center items-center px-6 text-center mt-5">
       <h1 className="text-2xl font-semibold mb-4">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 text-6xl font-bold">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-800 to-blue-500 text-6xl font-bold">
           27 million
         </span>
         <br />
@@ -21,20 +22,27 @@ export default function ConsentPage() {
       </h1>
 
       {/* Image */}
+
       <img src="/2.png" alt="Walking by Slimkit" className="w-48 h-auto mb-6" />
 
       {/* Consent Text */}
       <p className="text-xs text-gray-600 max-w-md mb-6">
         By pressing Continue I hereby agree to processing of my personal data.
-        Nothing is shared without my permission.
-        <span className=" underline">Privacy Policy</span> and 
-        <span className=" underline">Terms and Conditions apply</span>.
+        Nothing is shared without my permission.{" "}
+        <span className=" underline hover:cursor-pointer">
+          Privacy Policy
+        </span>{" "}
+        and{" "}
+        <span className=" underline hover:cursor-pointer">
+          Terms and Conditions
+        </span>{" "}
+        apply.
       </p>
 
       {/* Continue Button */}
       <button
         onClick={handleContinue}
-        className="bg-gradient-to-r from-blue-500 to-blue-700 text-yellow-300 font-semibold py-2 px-6 rounded-full shadow-md hover:scale-105 transition w-full max-w-xs text-2xl"
+        className="bg-gradient-to-r from-blue-800 to-blue-500 text-yellow-300 font-semibold py-2 px-6 rounded-full shadow-md hover:scale-105 transition w-full max-w-xs text-2xl"
       >
         Continue
       </button>
